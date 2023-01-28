2485509bdc21537bd5d4fbfc2caf850293803673
David LaRocco <dlarocco77@gmail.com>
2023-01-27 18:30:13 -0700

    add section transitions

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const navBtn = document.querySelectorAll('.control');
 const allSections = document.querySelector('body');
 
 function pageTransitions() {
+    // Event handler for navigation buttons
     navBtn.forEach(button => {
         button.addEventListener('click', event => {
             let currentBtn = document.querySelector('.active-btn');
@@ -11,6 +12,23 @@ function pageTransitions() {
             let newBtn = event.currentTarget;
             newBtn.classList.add('active-btn'); 
         });
+    });
+
+    // Event handler for section transitions
+    allSections.addEventListener('click', event => {
+        const btnElem = event.target.closest('.control');
+        const id = btnElem.dataset.id;
+        if(id) {
+            // hide other sections
+            sections.forEach(section => {
+                section.classList.remove('active');
+            });
+
+            // show selected section
+            const currentSection = document.getElementById(id);
+            currentSection.classList.add('active');
+        }
+
     });
 }
 
